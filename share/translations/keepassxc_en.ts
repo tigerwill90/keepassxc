c9a64be699f12fc2875c65cd592ec088ab853b23
outfoxxed <outfoxxed@outfoxxed.me>
2025-01-23 04:55:18 -0800

    Add --minimized CLI option

--- share/translations/keepassxc_en.ts
+++ share/translations/keepassxc_en.ts
@@ -9224,6 +9224,10 @@ This option is deprecated, use --set-key-file instead.</source>
         <source>Passkey</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>start minimized to the system tray</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QtIOCompressor</name>
